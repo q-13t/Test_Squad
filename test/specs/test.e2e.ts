a581a21584c057681e3e4bcc1e9d384519f7f548
q-13t <vova29092002@gmail.com>
2025-08-27 14:28:45 +0300

    L4: setValue & click

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -1,7 +1,7 @@
 import { expect, browser } from '@wdio/globals'
 
 describe('WebdriverIO main page', () => {
-    it('should have correct title', async () => {
+    xit('should have correct title', async () => {
         await browser.url('https://webdriver.io/');
         const title = await browser.getTitle();
 
@@ -10,7 +10,7 @@ describe('WebdriverIO main page', () => {
         await expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO');
     });
 
-    it ("should show addValue command", async () => {
+    xit ("should show addValue command", async () => {
         await browser.url('https://the-internet.herokuapp.com/login');  
 
         const input = await browser.$('#username');
@@ -21,7 +21,31 @@ describe('WebdriverIO main page', () => {
         await browser.pause(1000);
 
         await expect(input).toHaveValue('tomsmith123');
+    });
+
+    it("should show setValue command", async () => {
+        await browser.url('https://the-internet.herokuapp.com/login');
+
+        const input = await browser.$('#username');
+        await input.setValue('tomsmith');
+        await browser.pause(1000);
+
+        console.log(await input.getValue());
         
+        await expect(input).toHaveValue('tomsmith');
+    });
+    
+    it("Should click", async ()=>{
+        await browser.url('https://the-internet.herokuapp.com/login');
+    
+        const username = await browser.$('#username');
+        await username.setValue('tomsmith');
+        const password = await browser.$('#password');
+        await password.setValue('SuperSecretPassword!');
+        const button = await browser.$('.radius');
+        await button.click();
+    
+        await expect(browser).toHaveUrl('https://the-internet.herokuapp.com/secure');
     });
 })
 
