2ed25003ebf378d836c1ba29b4017414dce68674
q-13t <vova29092002@gmail.com>
2025-08-27 15:28:29 +0300

    L10: enabled & focused & scrolledInto

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -74,28 +74,49 @@ describe('WebdriverIO main page', () => {
 
         await expect(fresh_label).toHaveText("Свіже");
     });
-    it("Should be displayed",async()=>{
+    xit("Should be displayed",async()=>{
         await browser.url('https://webdriver.io/');
         const git_button = await browser.$('//*[@id="__docusaurus_skipToContent_fallback"]/header/div/div[1]/a[3]');
         await expect(git_button).toBeDisplayed();
     });
 
-    it("Should be visible",async()=>{
+    xit("Should be visible",async()=>{
         await browser.url('https://webdriver.io/');
         const git_button = await browser.$('//*[@id="__docusaurus_skipToContent_fallback"]/header/div/div[1]/a[3]');
         await expect(git_button).toBeDisplayedInViewport();
     });
 
-    it("Should not be visible",async()=>{
+    xit("Should not be visible",async()=>{
         await browser.url('https://webdriver.io/');
         const help_button = await browser.$('//*[@id="__docusaurus"]/footer/div/div[1]/div[1]/ul/li[4]/a');
         await expect(help_button).not.toBeDisplayedInViewport();
     });
 
-    it("Should be clickable",async()=>{
+    xit("Should be clickable",async()=>{
         await browser.url('https://webdriver.io/');
         const git_button = await browser.$('//*[@id="__docusaurus_skipToContent_fallback"]/header/div/div[1]/a[3]');
         await expect(git_button).toBeClickable();
     });
+
+    it("Should be enabled",async()=>{
+        await browser.url('https://webdriver.io/');
+        await expect(await browser.$('//*[@id="__docusaurus_skipToContent_fallback"]/header/div/div[1]/a[3]')).toBeEnabled();
+    });
+    it("To be focused",async()=>{
+        await browser.url('https://webdriver.io/');
+        const git_button = await browser.$('//*[@id="__docusaurus_skipToContent_fallback"]/header/div/div[1]/a[3]');
+        await expect(git_button).not.toBeFocused();
+        await git_button.click();
+        await expect(git_button).toBeFocused();
+    });
+
+    it("Should scrolled into",async()=>{
+        await browser.url('https://webdriver.io/');
+        const help_button = await browser.$('//*[@id="__docusaurus"]/footer/div/div[1]/div[1]/ul/li[4]/a');
+        await expect(help_button).not.toBeDisplayedInViewport();
+        await help_button.scrollIntoView();
+        await expect(help_button).toBeDisplayedInViewport();
+    });
+    // it("",async()=>{});
 })
 
